84a3980e11dc93afde1f4a76ab746ee96c4c53a9
Abhinandan789 <abhigautam7899@gmail.com>
2024-05-23 20:28:26 +0530

    warning added for devices lower than 700 px viewport

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,13 +5,17 @@ import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import { AuthContextProvider } from './context/AuthContext.jsx'
 import { SocketContextProvider } from './context/SocketContext.jsx'
+import DeviceDetection from '../DeviceDetection.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
         <SocketContextProvider>
-          <App />
+          <DeviceDetection>
+            <App />
+          </DeviceDetection>
+          
         </SocketContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
