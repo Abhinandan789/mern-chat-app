37cf9f72c114b27914762eede562fc840e1a2fe3
Abhinandan789 <abhigautam7899@gmail.com>
2024-05-23 18:56:01 +0530

    speed insights removed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import {Routes, Route, Navigate} from 'react-router-dom';
-//import './App.css'
 import Home from './pages/home/Home';
 import SignUp from './pages/signUp/SignUp';
 import LogIn from './pages/login/Login';
 import { Toaster } from 'react-hot-toast';
 import { useAuthContext } from './context/AuthContext';
-import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
 
@@ -18,7 +16,6 @@ function App() {
           <Route path="/signup" element={authUser ? <Navigate to='/' /> : <SignUp />} />
         </Routes>
         <Toaster />
-        <SpeedInsights />
       </div>
 
   )
