f4b5e0b068d334f0322f936dd6a169b118f625d7
Abhinandan789 <abhigautam7899@gmail.com>
2024-05-23 23:54:10 +0530

    ..

--- Frontend/src/DeviceDetection.jsx
+++ Frontend/src/DeviceDetection.jsx
@@ -22,8 +22,8 @@ const DeviceDetection = ({ children }) => {
     if (isSmallScreen) {
         return (
             <div className="flex flex-col items-center justify-center h-screen p-5 text-center w-full  shadow-md  backdrop-filter backdrop-blur-sm">
-                <p className="w-50 color-#1e2328 text-l text-center font-bold">
-                    Whoa, small screen {<img className='w-4 inline-block' src="./public/smartphone.png" alt="phone-icon" />} <br />  This feature works best on tablets or laptops with bigger screens. You can still access it here, but things might be a bit cramped. {<img className='w-5 -mt-2 inline-block' src="./public/rotate.png" alt="tilt-phn-gif"/>}
+                <p className="w-50 color-#050b10 text-l text-center font-bold">
+                    Whoa, small screen {<img className='w-4 inline-block' src="./public/smartphone.png" alt="phone-icon" />} <br />  This feature works best on tablets or laptops with bigger screens. You can still access it here, but things might be a bit cramped <br /> (You can {<img className='w-5 -mt-2 inline-block' src="./public/rotate.png" alt="tilt-phn-gif" />} or turn on desktop layout on your browser)
                 </p>
             </div>
         );
